93d636a598b6b128096e1e5f04a8295922edb89b
VictorEdM <thomasmannautor@gmail.com>
2022-10-31 20:36:54 -0300

    Student repository created

--- src/main/java/com/example/coursesspringdatajpa/CoursesSpringDataJpaApplication.java
+++ src/main/java/com/example/coursesspringdatajpa/CoursesSpringDataJpaApplication.java
@@ -1,13 +1,18 @@
 package com.example.coursesspringdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoursesSpringDataJpaApplication {
+public class CoursesSpringDataJpaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursesSpringDataJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
