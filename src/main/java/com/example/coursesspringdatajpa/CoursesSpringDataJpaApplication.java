e6beb977e7a7db076908e0243f2595362960bde7
VictorEdM <thomasmannautor@gmail.com>
2022-10-31 14:29:22 -0300

    Starting project

---
+++ src/main/java/com/example/coursesspringdatajpa/CoursesSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursesspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursesspringdatajpa/CoursesSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursesspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
